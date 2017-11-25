e5f7d7af2a02fda20e4217cc88fde653c49b0624
Keivon Arooji <karooji0@email.cpcc.edu>
2017-11-25 17:28:18 -0500

    api/friends POST, input validation

--- routing/apiRoutes.js
+++ routing/apiRoutes.js
@@ -7,4 +7,11 @@ module.exports = app => {
     app.get("/api/friends", (req, res) => {
         res.json(friendData);
     });
+
+    app.post("/api/friends", function(req, res) {
+        // Server will respond to requests and push validated users to friendData[]
+        // req.body is available since we're using the body-parser middleware
+        friendData.push(req.body);
+        res.json(req.body);
+    });
 }
